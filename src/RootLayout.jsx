485be65bf4f5e11f6f972e9b81c370189a1e9d52
ImranTheMan <ijason2002@gmail.com>
2024-05-15 03:13:26 +0800

    dagdagartehan

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -5,7 +5,7 @@ const RootLayout = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link className="navbar-brand" to="/">Affinity</Link>
+                <Link className="navbar-brand" to="/LandingPage">Affinity</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false"
