96e8f39112f29f4f0dfd305b10925ba264b2b2e4
RodPoli <142408199+RodPoli@users.noreply.github.com>
2024-05-16 15:23:25 +0800

    Responsiveness
    Added responsiveness for landingpage, datingapp, and corrected navbar responsiveness

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,36 +1,64 @@
-import {Link, Outlet} from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import React from "react";
 
 const RootLayout = () => {
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                
-                <Link className="navbar-brand" to="/LandingPage">Affinity</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/LandingPage">Landing Page</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/dating">Dating Page</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/Horoscope">Horoscope Page</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link
+          className="navbar-brand"
+          to="/LandingPage"
+          style={{ marginLeft: "4vw" }}
+        >
+          Affinity
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/LandingPage"
+                style={{ marginLeft: "5vw", marginRight: "-5vw" }}
+              >
+                Landing Page
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/dating"
+                style={{ marginLeft: "5vw", marginRight: "-5vw" }}
+              >
+                Affinity
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/Horoscope"
+                style={{ marginLeft: "5vw" }}
+              >
+                Horoscope Page
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-            <Outlet/>
-        </>
-    )
-}
+      <Outlet />
+    </>
+  );
+};
 
 export default RootLayout;
