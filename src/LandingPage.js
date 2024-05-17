034402c8c2ed7bd2edf73078a8dd4e3816c1997e
RodPoli <142408199+RodPoli@users.noreply.github.com>
2024-05-18 00:20:30 +0800

    Responsiveness (Without Navbar)
    Responsiveness for DatingApp and LandingPage.. No Responsiveness for navbar yet.

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './DatingApp.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <div className="container-fluid h-100">
-        <div className="row h-100">
+      <div className="container-fluid h-100 d-flex align-items-center justify-content-center">
+        <div className="row w-100">
           {/* Left Column for Content */}
-          <div className="col-md-6 d-flex align-items-center">
-            <div className="landing-page-content">
-              <h1 className="text-left">Welcome to Affinity</h1>
-              <p className="text-left">Your journey to finding your <br></br> perfect match begins here.</p>
-              <Link to="/dating" className="d-flex justify-content-center">
-                <button className="btn btn-primary text-left">START NOW</button>
+          <div className="col-lg-6 col-md-12 d-flex align-items-center justify-content-center">
+            <div className="landing-page-content text-center text-md-left">
+              <h1>Welcome to Affinity</h1>
+              <p>Your journey to finding your perfect match begins here.</p>
+              <Link to="/dating">
+                <button className="btn btn-primary">START NOW</button>
               </Link>
             </div>
           </div>
           {/* Right Column for Rotating Image */}
-          <div className="col-md-6 d-flex justify-content-end align-items-center">
-            <img src="white.png" alt="Horoscopes" className="horoscope-image" />
+          <div className="col-lg-6 col-md-12 d-flex justify-content-center">
+            <div className="image-container">
+              <img
+                src="white.png"
+                alt="Horoscopes"
+                className="horoscope-image"
+              />
+            </div>
           </div>
         </div>
       </div>
