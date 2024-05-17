b62c743572b9d4f2c79cff1cf0585fc348c4edc9
RodPoli <142408199+RodPoli@users.noreply.github.com>
2024-05-18 00:28:58 +0800

    Responsiveness for Navbar
    Fixed Responsiveness for Navbar without affecting other codes and modals

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,63 +1,30 @@
 import { Link, Outlet } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
+import { Navbar, Nav, NavItem, NavLink } from "react-bootstrap";
 
 const RootLayout = () => {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link
-          className="navbar-brand"
-          to="/LandingPage"
-          style={{ marginLeft: "4vw" }}
-        >
-          Affinity
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link
-                className="nav-link"
-                to="/LandingPage"
-                style={{ marginLeft: "2vw" }}
-              >
-                Landing Page
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className="nav-link"
-                to="/dating"
-                style={{ marginLeft: "2vw" }}
-              >
-                Affinity
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className="nav-link"
-                to="/Horoscope"
-                style={{ marginLeft: "2vw" }}
-              >
-                Horoscope Page
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="/LandingPage">Affinity</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="ml-auto">
+            <NavItem>
+              <NavLink href="/LandingPage">Landing Page</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/dating">Affinity</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Horoscope">Horoscope Page</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <Outlet />
-    </>
+    </div>
   );
 };
 
